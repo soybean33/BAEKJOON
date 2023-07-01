96cc90b4ee8136b41c990beb02ef107aa7c5f169
soybean33 <80668684+soybean33@users.noreply.github.com>
2023-07-01 14:52:35 +0900

    Update 11657 타임머신.cpp

--- 11657 타임머신.cpp
+++ 11657 타임머신.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 struct Edge{	// Edge: 간선 정보
-	int to;	// to: 도착 도시
+	int to;	// to: 도착도시
 	int time;	// time: 버스를 타고 이동하는데 걸리는 시간
 };
 
@@ -18,10 +18,14 @@ void solution(){
 	nodeTime[0] = 0;	// 출발 도시
 
 	for(int j=0; j<N; j++){	// N 번 반복
-		for(int i=0; i<N; i++){	// N 개의 도시 i 에 대하여
+
+		for(int i=0; i<N; i++){	// N 개의 도시 i 도시에 대하여
 			for(int k=0; k<v[i].size(); k++){	// i 도시에서 갈 수 있는 간선 정보
-				Edge next = v[i][k];	// next: 연결되어 있는 간선 정보
-				if(nodeTime[i] != 1e18 && nodeTime[next.to] > nodeTime[i] + next.time){	// i 도시에 이미 방문한 적이 있고, 이번에 탐색한 시간이 이전에 기록되어 있는 시간 보다 빠른 경우
+				Edge next = v[i][k];	// next: i 도시에 연결되어 있는 k 번째 간선 정보
+
+				if(nodeTime[i] == 1e18) continue;	// i 도시에 방문한 적이 없으면 continue
+
+				if(nodeTime[next.to] > nodeTime[i] + next.time){	// 이번에 탐색한 시간이 이전에 기록되어 있는 시간 보다 빠른 경우
 					nodeTime[next.to] = nodeTime[i] + next.time;	// 가장 빠른 시간 갱신
 
 					if(j == N -1) cycle = true;	// N 번째 탐색에서 값이 변하는 경우는 사이클이 있는 경우
@@ -40,9 +44,9 @@ void input(){
 	v.assign(N, std::vector<Edge>(0, {0, 0}));	// N 개의 도시에 대한 간선 정보 공간 할당
 
 	for(int i=0; i<M; i++){
-		int from, to, time;	// from: 시작도시, to: 도착도시, time: 버스를 타고 이동하는데 걸리는 시간
+		int from, to, time;	// from: 시작도시, to: 도착도시, time: 버스를 타고 이동하는데 걸리는 시간 혹은 타임머신으로 시간을 되돌아가는 경우
 		std::cin >> from >> to >> time;
-		v[from - 1].push_back({to - 1, time});
+		v[from - 1].push_back({to - 1, time});	// 도로와 타임머신은 단방향
 	}
 
 	return;
